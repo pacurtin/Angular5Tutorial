cce0157f6ecde10baaf9ee896c7382f3d822ce66
Paddy <paddy@travelloapp.com>
2017-11-09 15:44:50 +1000

    Experimented with the new router

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
+// Need this to do route parametrisation
+import {ActivatedRoute} from '@angular/router';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -8,7 +12,14 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.params.subscribe(res => console.log(res.id));
+  }
+
+  // Not really sure what the point of this is? Why not just put routerLink="" straight into the html?
+  sendMeHome() {
+    this.router.navigate(['']);
+  }
 
   ngOnInit() {
   }
